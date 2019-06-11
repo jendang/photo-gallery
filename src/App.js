9c19d9fd11e231c9bf1884cb94e60149e59661c0
jennydang <jenny.dang117@gmail.com>
2019-06-11 18:08:27 +0200

    fetching images successfully

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import axios from 'axios'
+import { clientId } from './api/unplash'
 import './App.css'
+import Gallery from './images/Gallery'
 
 const baseUrl = "https://api.unsplash.com"
 
 class App extends React.Component {
-  
-  render(){
+  state = {
+    images: []
+  }
+
+  componentDidMount() {
+    const response = axios.get(`${baseUrl}/photos/?client_id=${clientId}`)
+                          .then(response => response.data)
+                          .then(data => this.setState({ images: data }))
+                          .catch(err => console.error(err))
+    return response
+  }
+
+  renderImages = () => {
     return (
-      <div className="App">
-        Test
+      <Gallery 
+        images={this.state.images}
+      />
+
+    )
+  }
+  render(){
+    console.log(this.state)
+    return (
+      <div>
+        {this.renderImages()}
       </div>
     )
 
--- src/images/Gallery.js
+++ src/images/Gallery.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 class Gallery extends React.Component {
+
     render(){
         return (
             <div>
-                Gallery
+                {this.props.images.map(image => {
+                    return <img key={image.id} src={image.urls.thumb} alt={image.id} />
+                })}
             </div>
         )
     }
